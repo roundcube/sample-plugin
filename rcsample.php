8ee458c86bbe11a06d15053af9fa37b010676364
till <till@php.net>
2012-10-27 19:49:07 +0200

    Initial checkin.

---
+++ rcsample.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This doesn't do anything.
+ */
+class rcsample extends rcube_plugin
+{
+    public function init()
+    {
+        // start off here
+        // $rcmail = rcmail::get_instance();
+    }
+}
